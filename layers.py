c3418f6193eb3599540f4a8eca9441d884cfa387
xueyunlong <55629956+xueyunlong12589@users.noreply.github.com>
2021-11-20 21:09:58 +0800

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,1 +1,22 @@
+class GraphConvolution(nn.Module):
 
+    def __init__(self, num_in, num_out, bias=False):
+
+        super(GraphConvolution, self).__init__()
+
+        self.num_in = num_in
+        self.num_out = num_out
+        self.weight = nn.Parameter(torch.FloatTensor(num_in, num_out).cuda())
+        nn.init.kaiming_normal_(self.weight, mode='fan_in')
+        self.bias = None
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(num_out).cuda())
+            nn.init.uniform_(self.bias)
+
+    def forward(self, x, adj):
+        out = torch.matmul(adj, x)
+        out = torch.matmul(out, self.weight)
+        if self.bias is not None:
+            return out + self.bias
+        else:
+            return out
