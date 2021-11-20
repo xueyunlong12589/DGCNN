14dcac0da7aed2d1c69783015131e9f335361e73
xueyunlong <55629956+xueyunlong12589@users.noreply.github.com>
2021-11-20 21:39:30 +0800

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,3 +1,9 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn.modules.module import Module
+
+
 class GraphConvolution(nn.Module):
 
     def __init__(self, num_in, num_out, bias=False):
@@ -7,11 +13,11 @@ class GraphConvolution(nn.Module):
         self.num_in = num_in
         self.num_out = num_out
         self.weight = nn.Parameter(torch.FloatTensor(num_in, num_out).cuda())
-        nn.init.kaiming_normal_(self.weight, mode='fan_in')
+        nn.init.xavier_normal_(self.weight)
         self.bias = None
         if bias:
             self.bias = nn.Parameter(torch.FloatTensor(num_out).cuda())
-            nn.init.uniform_(self.bias)
+            nn.init.zeros_(self.bias)
 
     def forward(self, x, adj):
         out = torch.matmul(adj, x)
@@ -20,3 +26,14 @@ class GraphConvolution(nn.Module):
             return out + self.bias
         else:
             return out
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(Linear, self).__init__()
+        self.linear = nn.Linear(in_features, out_features, bias=bias)
+        nn.init.xavier_normal_(self.linear.weight)
+        if bias:
+            nn.init.zeros_(self.linear.bias)
+
+    def forward(self, inputs):
+        return self.linear(inputs)
