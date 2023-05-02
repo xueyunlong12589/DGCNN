89b96e07c348bcd980fa5333702ccab46cc46ccb
xueyunlong <55629956+xueyunlong12589@users.noreply.github.com>
2023-05-02 22:27:08 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,48 +4,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def normalize_A(A, symmetry=True):
-    A = F.relu(A)
+def normalize_A(A,lmax=2):
+    A=F.relu(A)
     N=A.shape[0]
     A=A*(torch.ones(N,N).cuda()-torch.eye(N,N).cuda())
-    if symmetry:
-        A = A + torch.transpose(A, 0, 1)
-        d = torch.sum(A, 1)
-        d = 1 / torch.sqrt(d + 1e-10)
-        D = torch.diag_embed(d)
-        L = torch.matmul(torch.matmul(D, A), D)
-    else:
-        d1 = torch.sum(A, 0)
-        d1 = 1 / torch.sqrt(d1 + 1e-10)
-        D1 = torch.diag_embed(d1)
-        d2 = torch.sum(A, 1)
-        d2 = 1 / torch.sqrt(d2 + 1e-10)
-        D2 = torch.diag_embed(d2)
-        L = torch.matmul(torch.matmul(D1, A), D2)
-    return L
+    A=A+A.T
+    d = torch.sum(A, 1)
+    d = 1 / torch.sqrt((d + 1e-10))
+    D = torch.diag_embed(d)
+    L = torch.eye(N,N).cuda()-torch.matmul(torch.matmul(D, A), D)
+    Lnorm=(2*L/lmax)-torch.eye(N,N).cuda()
+    return Lnorm
 
 
-def generate_adj(A, K,flag=2):
-    #A的次方
-    if flag==1:
-        support = []
-        for i in range(K):
-            if i == 0:
-                support.append(torch.eye(A.shape[-1]).cuda())
-            elif i == 1:
-                support.append(A)
-            else:
-                temp = torch.matmul(support[-1], A)
-                support.append(temp)
-    #chebynet
-    else:
-        support = []
-        for i in range(K):
-            if i == 0:
-                support.append(torch.eye(A.shape[-1]).cuda())
-            elif i == 1:
-                support.append(A)
-            else:
-                temp = torch.matmul(2*A,support[-1],)-support[-2]
-                support.append(temp)
+def generate_cheby_adj(L, K):
+    support = []
+    for i in range(K):
+        if i == 0:
+            support.append(torch.eye(L.shape[-1]).cuda())
+        elif i == 1:
+            support.append(L)
+        else:
+            temp = torch.matmul(2*L,support[-1],)-support[-2]
+            support.append(temp)
     return support
+
